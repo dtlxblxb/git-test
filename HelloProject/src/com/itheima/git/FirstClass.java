3fb7bc63f47cf5b909e90f8826bbeb7facdd4e4f
zhangsan <zhangsan@itcast.cn>
2020-05-07 20:42:16 +0800

    修改FirstClass

--- HelloProject/src/com/itheima/git/FirstClass.java
+++ HelloProject/src/com/itheima/git/FirstClass.java
@@ -3,5 +3,11 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
+        test();
+    }
+
+    private static void test() {
+        int i = 0;
+        System.out.println(i);
     }
 }
